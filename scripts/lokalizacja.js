22bb96f5244761520c7d2757a3f56851dcf92846
Maciej Masznicz <mk.masznicz@protonmail.com>
2024-01-05 19:17:17 +0100

    fsdfsfsd

--- scripts/lokalizacja.js
+++ scripts/lokalizacja.js
@@ -1,3 +1,11 @@
 function lokalizacja() {
     alert("W tej chwili nie możemy ustalić twojej lokalizacji.");
 }
+
+function kontakt() {
+    alert("W tej chwili nie możemy ustalić twojej lokalizacji.");
+}
+
+function lokalizacja() {
+    alert("W tej chwili nie możemy ustalić twojej lokalizacji.");
+}
